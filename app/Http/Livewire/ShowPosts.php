722f445736c827742baee877dce80481b6d92bd5
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-09 22:31:07 -0400

    Componentes de clase y propiedades
    Se aprendió el uso de los componentes de clase y como usar sus propiedades

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -7,7 +7,12 @@ use Livewire\Component;
 class ShowPosts extends Component
 {
     // --------------- Las propiedades enviadas al componente se reciben como atributos públicos en la clase
-    public $title;
+    public $titulo;
+
+    // --------------- Si se envían parámetros con diferente nombre, mount es un equivalente a setter
+    public function mount($title) {
+        $this->titulo = $title;
+    }
 
     // --------------- Este método renderiza el contenido dentro del componente show-posts
     public function render()
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -2,5 +2,6 @@
     {{-- Para que el renderizado reactivo funcione, todo el contenido debe estar dentro de un div padre --}}
     <h1>Hola mundo</h1>
 
-    {{ $title }}
+    {{-- Invocar a la propiedad de la clase --}}
+    {{ $titulo }}
 </div>
