8a1db58633d53db3291a4d09705085813e80dfad
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-17 15:48:47 -0400

    Ya que el componente ShowPosts actúa como conttrolador, se creó una variable pública search para un input de filtrado que se vincula en la vista y se hace l aquery correspondiente

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -8,10 +8,16 @@ use App\Models\Post;
 
 class ShowPosts extends Component
 {
+    // --------------- Propiedades del componente
+    public $search;
+
     // --------------- Este método renderiza el contenido dentro del componente show-posts
     public function render()
     {
-        $posts = Post::all();
+        // --------------- Campo de búsqueda por título y contenido
+        $posts = Post::where('title', 'like', "%{$this->search}%")
+                     ->orWhere('content', 'like', "%{$this->search}%")
+                     ->get();
 
         return view('livewire.show-posts', compact('posts'));
         // --------------- Se puede especificar el layout del que se extiende
