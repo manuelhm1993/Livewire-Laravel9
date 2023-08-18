742cca08c48e96cfb4a811b458f64ffbe5176068
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-18 05:22:14 -0400

    La forma de evitar que se renderize la página con cada acción es usar e lwire:model.defer, en el caso de llevar los datos del formulario, no es necesaria la renderización hasta que se envíen los datos

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -14,14 +14,19 @@
             <div class="mb-4">
                 {{-- Hacer uso de los componentes de jetstream para facilitar la maquetación --}}
                 <x-label value="Título del post" />
-                <x-input type="text" class="w-full" />
+
+                {{-- Sincronizar el formulario con las propiedades el componente --}}
+                <x-input type="text" class="w-full" wire:model.defer="title" />
+                {{ $title }}
             </div>
 
             <div class="mb-4">
                 <x-label value="Contenido del post" />
+                {{-- Para evitar el renderizado de la vista por cada letra que se escribe en los inputs se usa wire:model.defer --}}
                 
                 {{-- Utilizar las clases de tailwind para crear un form-control personal y aplicarlo a un texarea común --}}
-                <textarea name="" id="" rows="6" class="form-control w-full"></textarea>
+                <textarea name="" id="" rows="6" class="form-control w-full" wire:model.defer="content"></textarea>
+                {{ $content }}
             </div>
         </x-slot:content>
 
