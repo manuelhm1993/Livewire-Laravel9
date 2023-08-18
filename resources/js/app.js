5ecb7f1cfd71b2bacf70c00dc4a57d45ea646d30
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-18 06:32:21 -0400

    Instalación y configuración de sweetalert2 via npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,13 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
+import Swal from 'sweetalert2';
+
 window.Alpine = Alpine;
 
 Alpine.plugin(focus);
 
 Alpine.start();
+
+// Convierde a Swal en una variable global
+window.Swal = Swal;
