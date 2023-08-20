3c37546a3a778082b9c92ab72455ffce1a41ea76
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-19 21:54:07 -0400

    Juego de feedback con dos elementos al crear un post

--- resources/views/components/mh/spinner.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="animate-spin inline-block w-10 h-10 border-[3px] border-current border-t-transparent text-yellow-600 rounded-full"
-    role="status" aria-label="loading">
-    <span class="sr-only">Loading...</span>
-</div>
--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -40,10 +40,16 @@
                 Cancelar
             </x-secondary-button>
 
-            {{-- Botón de acción para crear un nuevo post --}}
-            <x-danger-button wire:click="store">
+            {{-- Escucha el click y llama al método store y en el proceso se remueve y muestra el spinner --}}
+            <x-danger-button wire:click="store" wire:loading.remove wire:target="store">
                 Crear post
             </x-danger-button>
+
+            {{-- wire:loading pone d-none al elemento y wire:target muestra el elemento en la acción especificada --}}
+            <div wire:loading wire:target="store" class="animate-spin inline-block w-10 h-10 border-[3px] border-current border-t-transparent text-red-600 rounded-full"
+                role="status" aria-label="loading">
+                <span class="sr-only">Loading...</span>
+            </div>
         </x-slot:footer>
     </x-dialog-modal>
 </div>
