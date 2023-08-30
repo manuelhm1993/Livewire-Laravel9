e2627624afc25401124318316e107c6a0e855dcf
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-30 05:24:08 -0400

    Lógica para actualizar el post, se hizo un algoritmo para validar si se seleccionó una nueva imagen, eliminar la imagen actual de la carpeta publica, actualizar el path con la url de la nueva imagen y luego guardarla en BBDD, el resto es casi igual a create, excepto que solo se resetean el open y la image, ya que title y content se requieren en el modal

--- app/Http/Livewire/EditPost.php
+++ app/Http/Livewire/EditPost.php
@@ -3,13 +3,21 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
+
+use App\MHClasses\Helper;
 use App\Models\Post;
+
+use Illuminate\Support\Facades\Storage;
 
 class EditPost extends Component
 {
+    use WithFileUploads;
+
     // Las propiedades se asignan dinámicamente desde la vista, pero se deben llamar igual que el parámetro
     public $post;
     public $open = false;
+    public $image, $identificador;
 
     // Permite vinvular las propiedades directamente en un input
     protected $rules = [
@@ -21,15 +29,28 @@ class EditPost extends Component
     public function mount(Post $post)
     {
         $this->post = $post;
+        $this->identificador = Helper::generateID();
     }
 
     public function update() {
+        // El método validate permite llenar el post con los nuevos datos si pasan la validación
         $this->validate();
+
+        // Validar si se seleccionó una nueva imagen
+        if($this->image) {
+            // Eliminar la imagen actual del post
+            Storage::disk('public')->delete($this->post->image);
+
+            // Guardar la nueva imagen en el directorio posts posts y modificar la url del post
+            $this->post->image = $this->image->store('posts', 'public');
+        }
  
         // Se usa save para actualizar, ya que guarda los cambios en el objeto Post
         $this->post->save();
 
-        $this->reset(['open']);
+        $this->reset(['open', 'image']);
+
+        $this->identificador = Helper::generateID();
 
         $this->emitTo('show-posts', 'render');
 
