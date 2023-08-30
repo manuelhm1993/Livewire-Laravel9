358e408ac1d7a9cba623f2c1d23b0fcc70e74a55
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-30 02:01:51 -0400

    Se creó una hoja de estilos para los botones, replicando las clases btn de bootstrap en tailwind. Se le dieron los estilos al enlace del componente edit-post y se importa desde el app.css. Se creó la vinculación en la clase EditPost con la propiedad post

--- app/Http/Livewire/EditPost.php
+++ app/Http/Livewire/EditPost.php
@@ -3,9 +3,19 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Post;
 
 class EditPost extends Component
 {
+    // Las propiedades se asignan dinámicamente desde la vista, pero se deben llamar igual que el parámetro
+    public $post;
+
+    // Si se desea realizar algún otro tratamiento, se usa el método mount en lugar de un constructor ya que solo se llama una vez
+    public function mount(Post $post)
+    {
+        $this->post = $post;
+    }
+
     public function render()
     {
         return view('livewire.edit-post');
--- resources/views/livewire/edit-post.blade.php
+++ resources/views/livewire/edit-post.blade.php
@@ -1,6 +1,5 @@
 <div>
-    <a href="#" class="text-indigo-600 hover:text-indigo-900">
-        Editar
+    <a class="btn btn-success">
         <i class="fa-solid fa-pen-to-square"></i>
     </a>
 </div>
