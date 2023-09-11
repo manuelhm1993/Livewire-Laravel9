90e3b529b08eaba6dcd77ec1385105c7f0d7c1c6
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-11 03:47:31 -0400

    Agregar spinner de carga de posts

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -24,6 +24,18 @@
                 @livewire('create-post')
             </div>
 
+            {{-- Spinner --}}
+            @if (!$readyToLoad)
+                <div class='flex items-center justify-center min-h-screen'>
+                    <button type="button" class="bg-blue-400 h-max w-max rounded-lg text-white font-bold hover:bg-blue-300 hover:cursor-not-allowed duration-[500ms,800ms]" disabled>
+                        <div class="flex items-center justify-center m-[10px]">
+                            <div class="h-5 w-5 border-t-transparent border-solid animate-spin rounded-full border-white border-4"></div>
+                            <div class="ml-2"> Cargando posts... <div>
+                        </div>
+                    </button>
+                </div>
+            @endif
+
             {{-- Mostrar el contenido si existen registros --}}
             @if (count($posts))
                 <table class="min-w-full leading-normal">
