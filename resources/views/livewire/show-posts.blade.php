adeb65b6cae40038681bc715515787063e28e56e
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-11 01:35:45 -0400

    Eliminar el header del app.layout, cuestión estética

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -1,11 +1,4 @@
 <div>
-    {{-- Para que el renderizado reactivo funcione, todo el contenido debe estar dentro de un div padre --}}
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     {{-- Aplicar el centrado del navigation-menu.blade.php --}}
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <x-mh.table>
